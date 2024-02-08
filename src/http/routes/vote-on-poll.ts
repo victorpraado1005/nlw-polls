8936e6a35455154c38c39a2d9551286e10869206
Victor Prado <prado.victor1005@gmail.com>
2024-02-08 17:30:46 -0300

    aula 3 nlw.

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,8 @@ import { FastifyInstance } from 'fastify';
 import { randomUUID } from 'crypto';
 import { prisma } from '../../lib/prisma';
 import { z } from 'zod';
+import { redis } from '../../lib/redis';
+import { voting } from '../../utils/voting-pub-sub';
 
 export async function voteOnPoll(app: FastifyInstance) {
   app.post('/polls/:pollId/votes', async (request, reply) => {
@@ -35,6 +37,13 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviouesVoteOnPoll.id,
           }
         })
+
+        const votes = await redis.zincrby(pollId, -1, userPreviouesVoteOnPoll.poolOptionId)
+
+        voting.publish(pollId, {
+          pollOptionId: userPreviouesVoteOnPoll.poolOptionId,
+          votes: Number(votes)
+        })
       } else if (userPreviouesVoteOnPoll) {
         return reply.status(400).send({
           message: 'You already voted on this poll'
@@ -61,6 +70,13 @@ export async function voteOnPoll(app: FastifyInstance) {
       }
     })
 
-    return reply.status(201).send({ sessionId })
+    const votes = await redis.zincrby(pollId, 1, pollOptionId)
+
+    voting.publish(pollId, {
+      pollOptionId,
+      votes: Number(votes)
+    })
+
+    return reply.status(201).send()
   })
 }
